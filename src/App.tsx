934e2c592c0e5bf306ec97411aea889006467a37
yangfei <yangfei86@vip.qq.com>
2021-06-10 21:26:29 +0800

    优化loading效果

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-// 首页组件
-import TodoList from './pages/TodoList/todoWrapper';
+// loading组件, 其他组件没有加载好的时候显示
+import Loading from './pages/Loading';
+// 首页组件, 懒加载（使用懒加载，loading动画才会生效，避免首页白屏的尴尬）
+const TodoList = lazy(() => import('./pages/TodoList/todoWrapper'));
 // 其他组件，懒加载
 const NotFound = lazy(() => import('./pages/NotFound'));
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>加载中...</div>}>
+      <Suspense fallback={<Loading />}>
         <Switch>
           <Route path={'/'} exact component={TodoList} />
           <Route path={'*'} component={NotFound} />
---
+++ src/pages/Loading/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.scss';
+
+const Loading: React.FC = () => {
+  return (
+    <div className={'loading'}>
+      <div className="sk-cube-grid">
+        <div className="sk-cube sk-cube1" />
+        <div className="sk-cube sk-cube2" />
+        <div className="sk-cube sk-cube3" />
+        <div className="sk-cube sk-cube4" />
+        <div className="sk-cube sk-cube5" />
+        <div className="sk-cube sk-cube6" />
+        <div className="sk-cube sk-cube7" />
+        <div className="sk-cube sk-cube8" />
+        <div className="sk-cube sk-cube9" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
