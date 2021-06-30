99114e7daf0b831ca6524c4c435407d4e51a44c7
Fei Yang (杨飞) <fei.yang@kyligence.io>
2021-06-30 23:18:19 +0800

    update travis (#23)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm -rf root@yangfei.org.cn:/var/www/build/*
-scp -o stricthostkeychecking=no -r ./dist/* root@yangfei.org.cn:/var/www/build
+scp -o stricthostkeychecking=no -r ../build/* root@yangfei.org.cn:/var/www/build
