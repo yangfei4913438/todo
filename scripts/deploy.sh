910699a14ce408a17d19a9dbb7c6606b1b97412d
Fei Yang (杨飞) <fei.yang@kyligence.io>
2021-07-01 01:08:17 +0800

    update travis (#30)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
 # 删除远程的文件
-rm -rf root@yangfei.org.cn:/var/www/build
+ssh root@yangfei.org.cn "rm -rf /var/www/*"
 
 # 将新创建的好的文件传到远程服务器
 scp -o stricthostkeychecking=no /tmp/build.tgz root@yangfei.org.cn:/var/www
 
-# 解压
-tar -zxf root@yangfei.org.cn:/var/www/build.tgz
-
-# 删除压缩包
-rm -f root@yangfei.org.cn:/var/www/build.tgz
+# 解压然后删除压缩包
+ssh root@yangfei.org.cn "tar -zxf /var/www/build.tgz"
 
 # 删除本地的压缩包
 rm -f /tmp/build.tgz
