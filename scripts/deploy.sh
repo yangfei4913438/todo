9ec99f2be3ca9ad55955a0491cbc6a49ba26fd83
Fei Yang (杨飞) <fei.yang@kyligence.io>
2021-06-30 23:40:56 +0800

    update travis (#25)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# 构建前端代码
+yarn build
+
+# 删除远程的文件
 rm -rf root@yangfei.org.cn:/var/www/build/*
+
+# 将新创建的好的文件传到远程服务器
 scp -o stricthostkeychecking=no -r ./build/* root@yangfei.org.cn:/var/www/build
