9ba7fb178ea4c651330e7626a8ce9dcd0153657d
Fei Yang (杨飞) <yangfei86@vip.qq.com>
2021-07-07 00:31:43 +0800

    remove script (#39)

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# 将新创建的好的文件传到远程服务器
-scp -o stricthostkeychecking=no /tmp/build.tgz root@yangfei.org.cn:/var/www
-
-# 删除远程的文件 解压 然后删除压缩包
-ssh -o stricthostkeychecking=no root@yangfei.org.cn "tar -zxf /var/www/build.tgz"
